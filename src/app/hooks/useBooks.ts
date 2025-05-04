423ffff69ea9bf35323057c1847707268709a1be
Juan Paulo Céspedes Méndez <jpcespedes@hotmail.com>
2025-05-03 20:10:53 -0500

    Improved localStorage functionality

--- src/app/hooks/useBooks.ts
+++ src/app/hooks/useBooks.ts
@@ -3,9 +3,6 @@ import books from '../books.json';
 
 export function useBooks() {
   const [selectedBook, setSelectedBook] = useState('');
-  const [readingList, setReadingList] = useState<string[]>([]);
-  const [alreadyReadList, setAlreadyReadList] = useState<string[]>([]);
-  const [wantToReadList, setWantToReadList] = useState<string[]>([]);
 
   const filterBooks = (searchTerm: string) => {
     return books.filter(
@@ -19,12 +16,26 @@ export function useBooks() {
   const addToList = (list: string[], setList: React.Dispatch<React.SetStateAction<string[]>>, listName: string) => {
     if (!list.includes(selectedBook)) {
       setList([...list, selectedBook]);
-      window.localStorage.setItem(listName, JSON.stringify([...list, selectedBook]));
+      saveToLocalStorage([...list, selectedBook], listName);
       console.log(`Added ${selectedBook} to ${listName} list`);
     } else {
       console.log(`${selectedBook} is already in the ${listName} list`);
     }
   };
+
+  const saveToLocalStorage = (list: string[], listName: string) => {
+    window.localStorage.setItem(listName, JSON.stringify(list));
+  }
+
+  const loadFromLocalStorage = (listName: string) => {
+    const list = window.localStorage.getItem(listName);
+    return list ? JSON.parse(list) : [];
+  };
+
+  const [readingList, setReadingList] = useState<string[]>(loadFromLocalStorage('reading'));
+  const [alreadyReadList, setAlreadyReadList] = useState<string[]>(loadFromLocalStorage('already read'));
+  const [wantToReadList, setWantToReadList] = useState<string[]>(loadFromLocalStorage('want to read'));
+
   
   return {
     selectedBook,
@@ -36,6 +47,7 @@ export function useBooks() {
     setWantToReadList,
     setSelectedBook,
     filterBooks,
-    addToList
+    addToList,
+    loadFromLocalStorage,
   };
 }
